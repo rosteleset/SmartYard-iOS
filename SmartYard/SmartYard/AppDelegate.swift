fff4d9bc943bab9190e332d4221b585f0eb8cf7f
Alexander Khiger <a.higer@madbrains.ru>
2020-01-28 18:35:31 +0400

    Добавлен пустой проект

---
+++ SmartYard/SmartYard/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  SmartYard
+//
+//  Created by admin on 28/01/2020.
+//  Copyright © 2020 Mad Brains. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
