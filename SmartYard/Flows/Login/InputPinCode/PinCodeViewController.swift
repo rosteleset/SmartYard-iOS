1fbeb59f426688c1ad213d7ee42a2473ce8de7cc
d.vorobev <d.vorobev@madbrains.ru>
2020-02-06 14:35:22 +0400

    [23] New group for pin code screen

---
+++ SmartYard/Flows/Login/InputPinCode/PinCodeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PinCodeViewController.swift
+//  SmartYard
+//
+//  Created by Mad Brains on 06.02.2020.
+//  Copyright © 2020 Mad Brains. All rights reserved.
+//
+
+import UIKit
+
+class PinCodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
---
+++ SmartYard/Flows/Login/InputPinCode/PinCodeViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PinCodeViewModel.swift
+//  SmartYard
+//
+//  Created by Mad Brains on 06.02.2020.
+//  Copyright © 2020 Mad Brains. All rights reserved.
+//
+
+import Foundation
+
+class PinCodeViewModel {
+    // TODO
+}
