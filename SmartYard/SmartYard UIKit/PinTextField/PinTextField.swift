441f7b1ef2f54e82b8799dd6ba352533343c2105
d.vorobev <d.vorobev@madbrains.ru>
2020-02-06 15:52:00 +0400

    [23] Renamed assets. Added PinTextField component

---
+++ SmartYard/SmartYard UIKit/PinTextField/PinTextField.swift
@@ -0,0 +1,120 @@
+//
+//  PinTextField.swift
+//  SmartYard
+//
+//  Created by Mad Brains on 06.02.2020.
+//  Copyright © 2020 Mad Brains. All rights reserved.
+//
+
+import UIKit
+import PMNibLinkableView
+import RxSwift
+import RxCocoa
+
+class PinTextField: PMNibLinkableView {
+
+    @IBOutlet private weak var containerView: UIView!
+    
+    @IBOutlet private weak var firstNumField: PinNumberField!
+    @IBOutlet private weak var secondNumField: PinNumberField!
+    @IBOutlet private weak var thirdNumField: PinNumberField!
+    @IBOutlet private weak var fourthNumField: PinNumberField!
+    
+    @IBOutlet private weak var fakeTextField: UITextField!
+    
+    private var numberViewsCollection: [PinNumberField] {
+        return [firstNumField, secondNumField, thirdNumField, fourthNumField]
+    }
+    
+    private let disposeBag = DisposeBag()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        configureFakeTextField()
+        addViewTapGesture()
+        bind()
+    }
+    
+    func fetchInputNumber() -> String? {
+        return fakeTextField.text
+    }
+    
+    func dismissKeybord() {
+        fakeTextField.resignFirstResponder()
+    }
+    
+    @objc private func didPressNumberField() {
+        if fakeTextField.text?.count == Constants.pinLength {
+            reset()
+        }
+        
+        fakeTextField.becomeFirstResponder()
+    }
+    
+    private func bind() {
+        fakeTextField.rx.text.changed
+            .subscribe(
+                onNext: { [weak self] text in
+                    guard let self = self else {
+                        return
+                    }
+                    
+                    self.fillNumberFields(with: text)
+                }
+            )
+            .disposed(by: disposeBag)
+    }
+    
+    private func addViewTapGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didPressNumberField))
+        containerView.addGestureRecognizer(tapGesture)
+    }
+    
+    private func fillNumberFields(with text: String?) {
+        numberViewsCollection.enumerated().forEach { offset, element in
+            guard element.fetchValue() != text?[safe: offset]?.string else {
+                return
+            }
+            
+            element.setNewValue(value: text?[safe: offset]?.string)
+        }
+    }
+    
+    private func reset() {
+        fakeTextField.clear()
+        
+        numberViewsCollection.forEach { view in
+            view.clear()
+        }
+    }
+    
+    private func configureFakeTextField() {
+        fakeTextField.delegate = self
+        fakeTextField.smartInsertDeleteType = UITextSmartInsertDeleteType.no
+        fakeTextField.keyboardType = .numberPad
+    }
+    
+}
+
+extension PinTextField: UITextFieldDelegate {
+    
+    func textField(
+        _ textField: UITextField,
+        shouldChangeCharactersIn range: NSRange,
+        replacementString string: String
+        ) -> Bool {
+        guard let textFieldText = textField.text,
+            let rangeOfTextToReplace = Range(range, in: textFieldText)
+            else {
+                return false
+        }
+        
+        let substringToReplace = textFieldText[rangeOfTextToReplace]
+        let count = textFieldText.count - substringToReplace.count + string.count
+        
+        return count <= Constants.pinLength
+    }
+    
+}
+
