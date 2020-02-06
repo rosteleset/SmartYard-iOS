a3d24e82b51836f4211c06e5b57d734b889066b8
Alexander Khiger <a.higer@madbrains.ru>
2020-02-06 15:35:37 +0400

    [31] Добавлено еще несколько элементов

--- SmartYard/Flows/MainTabBar/Home/Addresses/View/AddressesViewController.swift
+++ SmartYard/Flows/MainTabBar/Home/Addresses/View/AddressesViewController.swift
@@ -11,7 +11,11 @@ import RxSwift
 import RxCocoa
 
 class AddressesViewController: BaseViewController {
-
+    
+    @IBOutlet private weak var mainContainerView: UIView!
+    @IBOutlet private weak var addButton: UIButton!
+    @IBOutlet private weak var collectionView: UICollectionView!
+    
     let viewModel: AddressesViewModel
     
     init(viewModel: AddressesViewModel) {
@@ -22,6 +26,19 @@ class AddressesViewController: BaseViewController {
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+    }
+    
+    private func configureView() {
+        mainContainerView.cornerRadius = 24
+        mainContainerView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        
+        addButton.setImage(UIImage(named: "AddButtonIcon"), for: .normal)
+        addButton.setImage(UIImage(named: "AddButtonIcon")?.darkened(), for: .highlighted)
     }
 
 }
