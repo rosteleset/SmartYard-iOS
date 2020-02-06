bb551cfb1ac820cd404ab01aad5491aa50206d2c
d.vorobev <d.vorobev@madbrains.ru>
2020-02-06 11:44:07 +0400

    [21] Some code style fix

--- SmartYard/SmartYard UIKit/PhoneTextField/PhoneTextField.swift
+++ SmartYard/SmartYard UIKit/PhoneTextField/PhoneTextField.swift
@@ -136,19 +136,16 @@ class PhoneTextField: PMNibLinkableView {
 
 extension PhoneTextField: UITextFieldDelegate {
     
-    func textField(
-        _ textField: UITextField,
-        shouldChangeCharactersIn range: NSRange,
-        replacementString string: String
-    ) -> Bool {
-        
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         guard let textFieldText = textField.text,
-            let rangeOfTextToReplace = Range(range, in: textFieldText) else {
-                return false
+              let rangeOfTextToReplace = Range(range, in: textFieldText)
+        else {
+              return false
         }
         
         let substringToReplace = textFieldText[rangeOfTextToReplace]
         let count = textFieldText.count - substringToReplace.count + string.count
+        
         return count <= 10
     }
     
