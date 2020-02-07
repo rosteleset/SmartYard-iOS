f216fc981489889e8d0cf251203e276ad34ae9bc
d.vorobev <d.vorobev@madbrains.ru>
2020-02-07 14:42:13 +0400

    [32] Added empty classes

---
+++ SmartYard/Flows/AuthByContractNumViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AuthByContractNumViewController.swift
+//  SmartYard
+//
+//  Created by Mad Brains on 07.02.2020.
+//  Copyright © 2020 Mad Brains. All rights reserved.
+//
+
+import UIKit
+
+class AuthByContractNumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
