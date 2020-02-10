16f1e4b72febeeb2fcb98bc995d864badbf2096e
d.vorobev <d.vorobev@madbrains.ru>
2020-02-10 17:52:52 +0400

    [36] Layout for input address flow

---
+++ SmartYard/Flows/Address/InputAddressViewController.swift
@@ -0,0 +1,17 @@
+//
+//  InputAddressViewController.swift
+//  SmartYard
+//
+//  Created by Mad Brains on 10.02.2020.
+//  Copyright © 2020 Mad Brains. All rights reserved.
+//
+
+import UIKit
+
+class InputAddressViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
