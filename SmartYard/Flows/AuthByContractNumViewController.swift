4eacf0cb8e85b89661e6ced01ed06f328c0082a5
d.vorobev <d.vorobev@madbrains.ru>
2020-02-10 14:54:51 +0400

    [32] Implemented scroll view

--- SmartYard/Flows/AuthByContractNumViewController.swift
+++ SmartYard/Flows/AuthByContractNumViewController.swift
@@ -10,10 +10,20 @@ import UIKit
 
 class AuthByContractNumViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var containerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
+    
+//    override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+//        print("HERE: \(containerView.frame.size.width) ||| \(containerView.frame.size.height)")
+//        scrollView.contentSize = CGSize(
+//            width: containerView.frame.size.width,
+//            height: containerView.frame.size.height
+//        )
+//    }
 
 }
