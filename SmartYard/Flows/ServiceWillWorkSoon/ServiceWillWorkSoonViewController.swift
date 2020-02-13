26b8ac0c5e80f7dc63d83efbecc90e75e17922e7
d.vorobev <d.vorobev@madbrains.ru>
2020-02-13 14:51:23 +0400

    [46] Added empty screen

---
+++ SmartYard/Flows/ServiceWillWorkSoon/ServiceWillWorkSoonViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceWillWorkSoonViewController.swift
+//  SmartYard
+//
+//  Created by Mad Brains on 13.02.2020.
+//  Copyright © 2020 Mad Brains. All rights reserved.
+//
+
+import UIKit
+
+class ServiceWillWorkSoonViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ SmartYard/Flows/ServiceWillWorkSoon/ServiceWillWorkSoonViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ServiceWillWorkSoonViewModel.swift
+//  SmartYard
+//
+//  Created by Mad Brains on 13.02.2020.
+//  Copyright © 2020 Mad Brains. All rights reserved.
+//
+
+import Foundation
+
+class ServiceWillWorkSoonViewModel: BaseViewModel {
+    
+}
