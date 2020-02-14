b45434bc8af75db35730132c82cb77795cffe22b
d.vorobev <d.vorobev@madbrains.ru>
2020-02-14 10:42:16 +0400

    [47] Layout for vc

---
+++ SmartYard/Components/UI/RoundedView/FullRoundedView.swift
@@ -0,0 +1,23 @@
+//
+//  FullRoundedView.swift
+//  SmartYard
+//
+//  Created by Mad Brains on 14.02.2020.
+//  Copyright © 2020 Mad Brains. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FullRoundedView: UIView {
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        roundCorners(
+            [.topLeft, .topRight, .bottomLeft, .bottomRight],
+            radius: 20.0
+        )
+    }
+    
+}
