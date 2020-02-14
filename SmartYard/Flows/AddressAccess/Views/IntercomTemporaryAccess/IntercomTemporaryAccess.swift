73600fded6fab48d7db9266918aebeaec332645f
d.vorobev <d.vorobev@madbrains.ru>
2020-02-14 11:11:12 +0400

    [47] Intercom temporary access view

---
+++ SmartYard/Flows/AddressAccess/Views/IntercomTemporaryAccess/IntercomTemporaryAccess.swift
@@ -0,0 +1,21 @@
+//
+//  IntercomTemporaryAccess.swift
+//  SmartYard
+//
+//  Created by Mad Brains on 14.02.2020.
+//  Copyright © 2020 Mad Brains. All rights reserved.
+//
+
+import UIKit
+
+class IntercomTemporaryAccess: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
