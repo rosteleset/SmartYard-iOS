954d9eb79eb7cf6a00f1eb4b734065679faf697d
d.vorobev <d.vorobev@madbrains.ru>
2020-02-26 12:28:50 +0400

    [63] Address

---
+++ SmartYard/Services/APIService/Models/APIFullAddress.swift
@@ -0,0 +1,15 @@
+//
+//  APIFullAddress.swift
+//  SmartYard
+//
+//  Created by Mad Brains on 26.02.2020.
+//  Copyright © 2020 Mad Brains. All rights reserved.
+//
+
+import Foundation
+
+struct APIFullAddress: Decodable {
+    
+    let address: String
+    
+}
