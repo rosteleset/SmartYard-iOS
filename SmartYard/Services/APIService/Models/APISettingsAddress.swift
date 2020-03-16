339a2f64aac460fc934debb485c0584acdead63f
Alexander Khiger <a.higer@madbrains.ru>
2020-03-16 16:41:13 +0400

    [93] Добавлены новые поля в запрос getSettingsList

--- SmartYard/Services/APIService/Models/APISettingsAddress.swift
+++ SmartYard/Services/APIService/Models/APISettingsAddress.swift
@@ -11,11 +11,14 @@ struct APISettingsAddress: Decodable {
     let clientId: String?
     let clientName: String?
     let contractName: String?
+    let flatOwner: Bool?
+    let contractOwner: Bool?
+    let hasGates: Bool?
     let houseId: String?
     let flatId: String?
     let address: String
     let services: [String]
-    let roommates: [APIRoommate]?
+    let roommates: [APIRoommate]
     
     var servicesAvailability: [SettingsServiceType: Bool] {
         return [
@@ -31,6 +34,50 @@ struct APISettingsAddress: Decodable {
     
     var uniqueId: String {
         return (clientId ?? "") + (flatId ?? "")
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case clientId, clientName, contractName, flatOwner, contractOwner, hasGates, houseId, flatId, address
+        case services, roommates
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        clientId = try? container.decode(String.self, forKey: .clientId)
+        clientName = try? container.decode(String.self, forKey: .clientName)
+        contractName = try? container.decode(String.self, forKey: .contractName)
+        
+        let flatOwnerRawValue = try? container.decode(String.self, forKey: .flatOwner)
+        
+        switch flatOwnerRawValue {
+        case "t": flatOwner = true
+        case "f": flatOwner = false
+        default: flatOwner = nil
+        }
+        
+        let contractOwnerRawValue = try? container.decode(String.self, forKey: .contractOwner)
+        
+        switch contractOwnerRawValue {
+        case "t": contractOwner = true
+        case "f": contractOwner = false
+        default: contractOwner = nil
+        }
+        
+        let hasGatesRawValue = try? container.decode(String.self, forKey: .hasGates)
+        
+        switch hasGatesRawValue {
+        case "t": hasGates = true
+        case "f": hasGates = false
+        default: hasGates = nil
+        }
+        
+        houseId = try? container.decode(String.self, forKey: .houseId)
+        flatId = try? container.decode(String.self, forKey: .flatId)
+        address = try container.decode(String.self, forKey: .address)
+        services = try container.decode([String].self, forKey: .services)
+        
+        roommates = (try? container.decode([APIRoommate].self, forKey: .roommates)) ?? []
     }
     
 }
