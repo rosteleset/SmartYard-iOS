a2e17676bfb2a8e4481fd5d0335ad284bd3c02ba
d.vorobev <d.vorobev@madbrains.ru>
2020-03-18 11:56:01 +0400

    [99] Layout

---
+++ SmartYard/Flows/Login/ResetPassword/ResetPasswordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResetPasswordViewController.swift
+//  SmartYard
+//
+//  Created by Mad Brains on 18.03.2020.
+//  Copyright © 2020 Mad Brains. All rights reserved.
+//
+
+import UIKit
+
+class ResetPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
