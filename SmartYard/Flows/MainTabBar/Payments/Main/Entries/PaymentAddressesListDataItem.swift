e280440b6892198dfd2d95a6355f74d8f1e96b6b
d.vorobev <d.vorobev@madbrains.ru>
2020-04-02 12:56:18 +0400

    [122] Temp

---
+++ SmartYard/Flows/MainTabBar/Payments/Main/Entries/PaymentAddressesListDataItem.swift
@@ -0,0 +1,9 @@
+//
+//  PaymentAddressesListDataItem.swift
+//  SmartYard
+//
+//  Created by Mad Brains on 02.04.2020.
+//  Copyright © 2020 Mad Brains. All rights reserved.
+//
+
+import Foundation
