d67c9eebb72ae2b78fbf528f53fd15cac8f6bcad
d.vorobev <d.vorobev@madbrains.ru>
2020-04-02 12:56:18 +0400

    [122] Temp

---
+++ SmartYard/Flows/MainTabBar/Payments/Main/View/Cell/PaymentAddressCell.swift
@@ -0,0 +1,24 @@
+//
+//  PaymentAddressCell.swift
+//  SmartYard
+//
+//  Created by Mad Brains on 02.04.2020.
+//  Copyright © 2020 Mad Brains. All rights reserved.
+//
+
+import UIKit
+
+class PaymentAddressCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- SmartYard/Flows/MainTabBar/Payments/Main/View/PaymentsViewController.swift
+++ SmartYard/Flows/MainTabBar/Payments/Main/View/PaymentsViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import JGProgressHUD
 
-class PaymentsViewController: BaseViewController {
+class PaymentsViewController: BaseViewController, LoaderPresentable {
 
+    @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private weak var skeletonContainer: UIView!
+    
+    private var refreshControl = UIRefreshControl()
+    
     private let viewModel: PaymentsViewModel
+    
+    var loader: JGProgressHUD?
     
     init(viewModel: PaymentsViewModel) {
         self.viewModel = viewModel
@@ -22,6 +30,32 @@ class PaymentsViewController: BaseViewController {
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+        configureTableView()
+        bind()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if skeletonContainer.isSkeletonActive {
+            skeletonContainer.showSkeletonAsynchronously()
+        }
+    }
+    
+    private func configureTableView() {
+        tableView.refreshControl = refreshControl
+        refreshControl.tintColor = UIColor.SmartYard.gray
+        
+        tableView.register(nibWithCellClass: PaymentAddressCell.self)
+    }
+    
+    private func configureView() {
+        
     }
 
 }
