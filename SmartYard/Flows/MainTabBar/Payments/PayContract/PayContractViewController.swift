5e3359f44bdafc4301962be5cb9bbe0c2cc4f738
d.vorobev <d.vorobev@madbrains.ru>
2020-04-03 13:39:17 +0400

    [122] Dots segment view

---
+++ SmartYard/Flows/MainTabBar/Payments/PayContract/PayContractViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PayContractViewController.swift
+//  SmartYard
+//
+//  Created by Mad Brains on 03.04.2020.
+//  Copyright © 2020 Mad Brains. All rights reserved.
+//
+
+import UIKit
+
+class PayContractViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
