efbe480c2df580002829b8e8106be342b55dc45f
d.vorobev <d.vorobev@madbrains.ru>
2020-04-03 13:39:17 +0400

    [122] Dots segment view

---
+++ SmartYard/Components/UI/DotsSegmentView/DotsSegmentView.swift
@@ -0,0 +1,30 @@
+//
+//  DotsSegmentView.swift
+//  SmartYard
+//
+//  Created by Mad Brains on 03.04.2020.
+//  Copyright © 2020 Mad Brains. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DotsSegmentView: UIView {
+    
+    @IBOutlet private weak var leftDotView: UIView!
+    @IBOutlet private weak var centerDotView: UIView!
+    @IBOutlet private weak var rightDotView: UIView!
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        leftDotView.cornerRadius = leftDotView.frame.size.width / 2
+        centerDotView.cornerRadius = centerDotView.frame.size.width / 2
+        rightDotView.cornerRadius = rightDotView.frame.size.width / 2
+    }
+    
+    func updateContentOffset() {
+        
+    }
+    
+}
