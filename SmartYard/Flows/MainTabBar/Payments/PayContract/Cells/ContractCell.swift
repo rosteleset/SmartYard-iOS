fd41660d4a5809c3ef5e0731fa0d8439c04f0cd6
d.vorobev <d.vorobev@madbrains.ru>
2020-04-03 15:35:57 +0400

    [122] Contract pay cell

---
+++ SmartYard/Flows/MainTabBar/Payments/PayContract/Cells/ContractCell.swift
@@ -0,0 +1,17 @@
+//
+//  ContractCell.swift
+//  SmartYard
+//
+//  Created by Mad Brains on 03.04.2020.
+//  Copyright © 2020 Mad Brains. All rights reserved.
+//
+
+import UIKit
+
+class ContractCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
