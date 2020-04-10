62ac7202f9a3d8ff4c721766de22c81775b2adb2
d.vorobev <d.vorobev@madbrains.ru>
2020-04-10 16:19:25 +0400

    [135] Code review

--- SmartYardWdg/View/WidgetViewController.swift
+++ SmartYardWdg/View/WidgetViewController.swift
@@ -22,7 +22,7 @@ class WidgetViewController: UIViewController, NCWidgetProviding {
     private let areObjectsGrantAccessed = BehaviorSubject<[Int: Bool]>(value: [:])
     private let doorOpened = PublishSubject<Int?>()
     
-    let disposeBag = DisposeBag()
+    private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
