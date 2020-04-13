7202e2cfd356033f65b41d47cf84aace3095ec35
d.vorobev <d.vorobev@madbrains.ru>
2020-04-13 12:35:40 +0400

    [140] Container layout

---
+++ SmartYard/Flows/MainTabBar/Home/Cameras/SelectCameraContainer/SelectCameraContainerViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SelectCameraContainerViewController.swift
+//  SmartYard
+//
+//  Created by Mad Brains on 13.04.2020.
+//  Copyright © 2020 Mad Brains. All rights reserved.
+//
+
+import UIKit
+
+class SelectCameraContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
