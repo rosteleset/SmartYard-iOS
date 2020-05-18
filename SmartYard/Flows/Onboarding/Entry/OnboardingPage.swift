beae9847c40fb4f4f8bcef9ef0d9d7b75bd92701
d.vorobev <d.vorobev@madbrains.ru>
2020-05-18 12:48:31 +0400

    [170] Added onboarding files

---
+++ SmartYard/Flows/Onboarding/Entry/OnboardingPage.swift
@@ -0,0 +1,48 @@
+//
+//  OnboardingPages.swift
+//  SmartYard
+//
+//  Created by Mad Brains on 21.04.2020.
+//  Copyright © 2020 Mad Brains. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum OnboardingPage: Int, CaseIterable {
+    
+    case cctv
+    case fullControl
+    case smartYard
+    
+    var image: UIImage? {
+        switch self {
+        case .cctv: return UIImage(named: "FirstOnboardingPageIcon")
+        case .fullControl: return UIImage(named: "SecondOnboardingPageIcon")
+        case .smartYard: return UIImage(named: "ThirdOnboardingPageIcon")
+        }
+    }
+    
+    var titleText: String {
+        switch self {
+        case .cctv: return "Видеонаблюдение"
+        case .fullControl: return "Все под контролем"
+        case .smartYard: return "Умный двор"
+        }
+    }
+    
+    var subTitleText: String {
+        switch self {
+        case .cctv: return "Будь в курсе всего, что происходит возле дома"
+        case .fullControl: return "Управляй услугами и оплачивай их через приложение"
+        case .smartYard: return "Управляй домофоном, воротами или шлагбаумом со смартфона"
+        }
+    }
+
+}
+//
+//extension OnboardingPage {
+//
+//    static let allPages: [OnboardingPage] = [.cctv, .fullControl, .smartYard]
+//
+//}
