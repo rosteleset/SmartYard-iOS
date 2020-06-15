23e7b25b08f890739c2920aefb29b5469c7a779b
Alexander Khiger <a.higer@madbrains.ru>
2020-06-15 19:48:07 +0400

    [258] Наработки по переносу в контроллеры

--- SmartYard/Flows/MainTabBar/Home/Cameras/ArchivePage/View/ArchivePageViewController.swift
+++ SmartYard/Flows/MainTabBar/Home/Cameras/ArchivePage/View/ArchivePageViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import JTAppleCalendar
 
 class ArchivePageViewController: BaseViewController {
     
+    @IBOutlet private weak var calendarView: JTACMonthView!
+    @IBOutlet private weak var monthLabel: UILabel!
+    @IBOutlet private weak var leftArrowButton: UIButton!
+    @IBOutlet private weak var rightArrowButton: UIButton!
+    
     private let viewModel: ArchivePageViewModel
+    
+    private let formatter = DateFormatter()
+    private let currentCalendar = Calendar.current
+    
+    private let endDate = Date()
+    
+    private var startDate: Date {
+        return endDate.adding(.day, value: -7)
+    }
     
     init(viewModel: ArchivePageViewModel) {
         self.viewModel = viewModel
@@ -27,12 +42,202 @@ class ArchivePageViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureCalendarView()
+        setupCalendar()
+        
         bind()
     }
-
-    func bind() {
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        let visibleDates = calendarView.visibleDates()
+        calendarView.viewWillTransition(to: size, with: coordinator, anchorDate: visibleDates.monthDates.first?.date)
+        setupCalendar()
+    }
+    
+    func setupCalendar() {
+        setupCalendarHeader(from: calendarView.visibleDates())
+    }
+    
+    fileprivate func configureCell(view: JTACDayCell?, cellState: CellState) {
+        guard let myCustomCell = view as? CustomDayCell else {
+            return
+        }
+        
+        myCustomCell.configure(
+            with: cellState,
+            isValidDate: cellState.date.isBetween(startDate, endDate, includeBounds: true)
+        )
+    }
+    
+    private func setupCalendarHeader(from visibleDates: DateSegmentInfo) {
+        guard let visibleDate = visibleDates.monthDates.first?.date else {
+            return
+        }
+        
+        // MARK: Заголовок
+        
+        formatter.dateFormat = "LLLL"
+        
+        let nameOfMonth = formatter.string(from: visibleDate).capitalized
+        let year = currentCalendar.component(.year, from: visibleDate)
+        
+        monthLabel.text = nameOfMonth + " " + String(year)
+        
+        // MARK: Показ и скрытие стрелочек
+        
+        let startDateMonth = startDate.month
+        let endDateMonth = endDate.month
+        let visibleDateMonth = visibleDate.month
+        
+        leftArrowButton.isHidden = visibleDateMonth <= startDateMonth
+        rightArrowButton.isHidden = visibleDateMonth >= endDateMonth
+    }
+    
+    private func configureCalendarView() {
+        calendarView.register(nibWithCellClass: CustomDayCell.self)
+        
+        let headerNib = UINib(nibName: "WhiteSectionHeaderView", bundle: Bundle.main)
+        calendarView.register(
+            headerNib,
+            forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+            withReuseIdentifier: "WhiteSectionHeaderView"
+        )
+        
+        calendarView.calendarDelegate = self
+        calendarView.calendarDataSource = self
+        
+        calendarView.scrollingMode = .stopAtEachCalendarFrame
+        calendarView.minimumLineSpacing = 0
+        calendarView.minimumInteritemSpacing = 0
+        
+        calendarView.scrollToDate(Date())
+        
+        leftArrowButton.rx
+            .tap
+            .asDriver()
+            .drive(
+                onNext: { [weak self] in
+                    self?.calendarView.scrollToSegment(.previous)
+                }
+            )
+            .disposed(by: disposeBag)
+        
+        rightArrowButton.rx
+            .tap
+            .asDriver()
+            .drive(
+                onNext: { [weak self] in
+                    self?.calendarView.scrollToSegment(.next)
+                }
+            )
+            .disposed(by: disposeBag)
+    }
+    
+    private func bind() {
         let input = ArchivePageViewModel.Input()
         let output = viewModel.transform(input)
     }
     
+}
+
+extension ArchivePageViewController: JTACMonthViewDataSource, JTACMonthViewDelegate {
+
+    func calendar(
+        _ calendar: JTACMonthView,
+        headerViewForDateRange range: (start: Date, end: Date),
+        at indexPath: IndexPath
+    ) -> JTACMonthReusableView {
+        // swiftlint:disable force_cast
+        return calendar.dequeueReusableJTAppleSupplementaryView(
+            withReuseIdentifier: "WhiteSectionHeaderView",
+            for: indexPath
+        ) as! WhiteSectionHeaderView
+    }
+
+    func calendarSizeForMonths(_ calendar: JTACMonthView?) -> MonthSize? {
+        return MonthSize(defaultSize: 40)
+    }
+
+    func calendar(
+        _ calendar: JTACMonthView,
+        willDisplay cell: JTACDayCell,
+        forItemAt date: Date,
+        cellState: CellState,
+        indexPath: IndexPath
+    ) {
+        configureCell(view: cell, cellState: cellState)
+    }
+
+    func calendar(
+        _ calendar: JTACMonthView,
+        cellForItemAt date: Date,
+        cellState: CellState,
+        indexPath: IndexPath
+    ) -> JTACDayCell {
+        // swiftlint: force_cast
+        let cell = calendar.dequeueReusableJTAppleCell(
+            withReuseIdentifier: "CustomDayCell",
+            for: indexPath
+        ) as! CustomDayCell
+
+        configureCell(view: cell, cellState: cellState)
+
+        return cell
+    }
+
+    func configureCalendar(_ calendar: JTACMonthView) -> ConfigurationParameters {
+        formatter.dateFormat = "yyyy MM dd"
+        formatter.timeZone = currentCalendar.timeZone
+        formatter.locale = .init(identifier: "RU")
+
+        let parameters = ConfigurationParameters(
+            startDate: startDate,
+            endDate: endDate,
+            numberOfRows: 6,
+            calendar: currentCalendar,
+            generateInDates: .forAllMonths,
+            generateOutDates: .tillEndOfGrid,
+            firstDayOfWeek: .monday,
+            hasStrictBoundaries: true
+        )
+
+        return parameters
+    }
+
+    func calendar(
+        _ calendar: JTACMonthView,
+        shouldSelectDate date: Date,
+        cell: JTACDayCell?,
+        cellState: CellState,
+        indexPath: IndexPath
+    ) -> Bool {
+        return date.isBetween(startDate, endDate, includeBounds: true)
+    }
+
+    func calendar(
+        _ calendar: JTACMonthView,
+        didSelectDate date: Date,
+        cell: JTACDayCell?,
+        cellState: CellState,
+        indexPath: IndexPath
+    ) {
+        configureCell(view: cell, cellState: cellState)
+
+//        delegate?.archiveView(self, didSelectDate: date)
+    }
+
+    func calendar(
+        _ calendar: JTACMonthView,
+        didDeselectDate date: Date,
+        cell: JTACDayCell?,
+        cellState: CellState,
+        indexPath: IndexPath
+    ) {
+        configureCell(view: cell, cellState: cellState)
+    }
+
+    func calendar(_ calendar: JTACMonthView, didScrollToDateSegmentWith visibleDates: DateSegmentInfo) {
+        setupCalendarHeader(from: visibleDates)
+    }
+
 }
--- SmartYard/Flows/MainTabBar/Home/Cameras/OnlinePage/View/OnlinePageViewController.swift
+++ SmartYard/Flows/MainTabBar/Home/Cameras/OnlinePage/View/OnlinePageViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import AVKit
 
 class OnlinePageViewController: BaseViewController {
     
+    @IBOutlet private weak var collectionView: UICollectionView!
+    @IBOutlet private weak var scrollView: UIScrollView!
+    @IBOutlet private weak var cameraContainer: UIView!
+    
+    private var playerViewController: AVPlayerViewController?
+    private var player: AVPlayer?
+    
+    @IBOutlet private var collectionViewHeightConstraint: NSLayoutConstraint!
+    
     private let viewModel: OnlinePageViewModel
+    
+    private var cameras = [CameraObject]()
+    private var selectedCameraNumber: Int?
     
     init(viewModel: OnlinePageViewModel) {
         self.viewModel = viewModel
@@ -27,12 +40,163 @@ class OnlinePageViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configurePlayer()
+        configureCollectionView()
+        
         bind()
     }
-
-    func bind() {
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        try? AVAudioSession.sharedInstance().setCategory(.playback)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        playerViewController?.view.frame = cameraContainer.bounds
+    }
+    
+    func setCameras(_ cameras: [CameraObject], selectedCamera: CameraObject?) {
+        self.cameras = cameras
+        
+        collectionView.reloadData { [weak self] in
+            guard let selectedCamera = selectedCamera,
+                let index = cameras.firstIndex(of: selectedCamera) else {
+                    return
+            }
+            
+            let indexPath = IndexPath(row: index, section: 0)
+            
+            self?.collectionView.selectItem(
+                at: indexPath,
+                animated: false,
+                scrollPosition: .top
+            )
+            
+            self?.reloadCameraIfNeeded(selectedIndexPath: indexPath)
+        }
+    }
+    
+    private func bind() {
         let input = OnlinePageViewModel.Input()
         let output = viewModel.transform(input)
+    }
+    
+    private func configurePlayer() {
+        let playerViewController = AVPlayerViewController()
+        playerViewController.videoGravity = .resizeAspect
+        self.playerViewController = playerViewController
+        
+        let player = AVPlayer()
+        playerViewController.player = player
+        self.player = player
+        
+        addChild(playerViewController)
+        cameraContainer.addSubview(playerViewController.view)
+        playerViewController.didMove(toParent: self)
+    }
+    
+    private func configureCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        collectionView.register(nibWithCellClass: CameraNumberCell.self)
+        
+        collectionView.rx
+            .observeWeakly(CGSize.self, "contentSize")
+            .subscribe(
+                onNext: { [weak self] size in
+                    guard let self = self, let uSize = size else {
+                        return
+                    }
+                    
+                    self.collectionViewHeightConstraint.constant = uSize.height
+                    self.view.setNeedsLayout()
+                }
+            )
+            .disposed(by: disposeBag)
+    }
+    
+    private func reloadCameraIfNeeded(selectedIndexPath: IndexPath) {
+        let camera = cameras[selectedIndexPath.row]
+        
+        print("Selected Camera #\(camera.cameraNumber)")
+        
+        guard camera.cameraNumber != selectedCameraNumber else {
+            return
+        }
+        
+        selectedCameraNumber = camera.cameraNumber
+        
+//        delegate?.onlineView(self, didSelectCamera: camera)
+        
+        player?.replaceCurrentItem(with: AVPlayerItem(url: camera.video))
+        player?.play()
+    }
+    
+}
+
+extension OnlinePageViewController: UICollectionViewDataSource {
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cameras.count
+    }
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withClass: CameraNumberCell.self, for: indexPath)
+
+        cell.configure(curCamera: cameras[indexPath.row])
+
+        return cell
+    }
+
+}
+
+extension OnlinePageViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
+        return CGSize(width: 36, height: 36)
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        minimumInteritemSpacingForSectionAt section: Int
+    ) -> CGFloat {
+        return 28
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        minimumLineSpacingForSectionAt section: Int
+    ) -> CGFloat {
+        return 24
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        insetForSectionAt section: Int
+    ) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 18, left: 0, bottom: 18, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        reloadCameraIfNeeded(selectedIndexPath: indexPath)
     }
     
 }
