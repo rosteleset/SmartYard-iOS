5a28cfdf83701f5bd2ea81044230d28d60d328d0
Alexander Khiger <a.higer@madbrains.ru>
2020-06-30 14:24:14 +0400

    [195] more tests

--- SmartYard/AppDelegate.swift
+++ SmartYard/AppDelegate.swift
@@ -10,6 +10,26 @@ import UIKit
 import Firebase
 import YandexMobileMetrica
 
+extension UIViewController {
+    
+    var topViewController: UIViewController? {
+        if let navigationController = self as? UINavigationController {
+            return navigationController.visibleViewController?.topViewController
+        }
+        
+        if let tabBarController = self as? UITabBarController {
+            return tabBarController.selectedViewController?.topViewController
+        }
+        
+        if let presentedController = presentedViewController {
+            return presentedController.topViewController
+        }
+        
+        return self
+    }
+    
+}
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
