d31bf6ef044afbe48b613dd8003eb07f1a4dc4e4
chekmari <stirinmore@gmail.com>
2026-03-10 16:45:34 +0300

    refactor: remove debug RxSwift example code from BaseViewController
    BaseViewController contained temporary example code with RxSwift subjects, observables and unused UIKit elements that were left from experiments or debugging.
    
    - removed RxSwift-related imports
    - removed unused viewWillAppear override
    - removed example RxSwift chains and subscriptions
    - cleaned BaseViewController to keep only its base responsibility

--- SmartYard/Components/Base Classes/BaseViewController.swift
+++ SmartYard/Components/Base Classes/BaseViewController.swift
@@ -7,71 +7,8 @@
 //
 
 import UIKit
-import RxSwift //
-import RxCocoa //
-import RxRelay //
-import RxDataSources // Diffabale
 
 class BaseViewController: UIViewController, HasDisposeBag {
 
     override var preferredStatusBarStyle: UIStatusBarStyle { .lightContent }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-
-    }
-
-    func someFunction() {
-        let button = UIButton()
-
-        let observable = Observable.just(1)
-
-        let publishSubject = PublishSubject<[Int]>()
-        let behaviorSubject = BehaviorSubject<[Int]>(value: [1])
-
-        behaviorSubject
-            .distinctUntilChanged()
-            .filter { array in
-                array[0] % 2 == 0
-            }
-            .mapToVoid()
-            .subscribe {
-
-            }
-            .disposed(by: disposeBag)
-
-        behaviorSubject.onNext([0])
-        behaviorSubject.onNext([0])
-
-        behaviorSubject.onNext([2, 3])
-        behaviorSubject.onNext([5, 3])
-
-
-
-        behaviorSubject
-            .subscribe { number in
-                print(number)
-            }
-            .disposed(by: disposeBag)
-
-        observable
-            .subscribe { number in
-                print(number)
-            }
-            .disposed(by: disposeBag)
-
-        observable
-            .subscribe { number in
-                print(number)
-            }
-            .disposed(by: disposeBag)
-
-        observable
-            .subscribe { number in
-                print(number)
-            }
-            .disposed(by: disposeBag)
-
-    }
-
 }
